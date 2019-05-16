fb3f9ba5c720c41f425832d14ae39dee6dd6c33c
Alexey Gasenko <gboydf@yandex.ru>
2019-05-16 15:36:07 +0500

    Add schemas for Statistics

---
+++ models/Driver.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const DriverSeasonSchema = new Schema({
+  year: {
+    type: Number,
+    required: true
+  },
+  points: {
+    type: Number,
+    required: true
+  }
+});
+
+const DriverSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  surname: {
+    type: String,
+    required: true
+  },
+  photo: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  countryFlag: {
+    type: String,
+    required: true
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true
+  },
+  dateOfDeath: {
+    type: Date
+  },
+  yearStart: {
+    type: Number,
+    required: true
+  },
+  yearEnd: {
+    type: Number
+  },
+  teams: {
+    type: mongoose.Schema.Types.ObjectId, ref: 'teams'
+  },
+  season: {
+    type: [DriverSeasonSchema]
+  }
+});
+
+const Driver = mongoose.model('drivers', DriverSchema);
+
+module.exports = Driver;
---
+++ models/Statistics.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const StatisticsSchema = new Schema({
+  year: {
+    type: Number,
+    required: true
+  },
+  teams: [{ type: mongoose.Schema.Types.ObjectId, ref: 'teams' }],
+  drivers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'drivers' }]
+});
+
+const Statistics = mongoose.model('statistics', StatisticsSchema);
+
+module.exports = Statistics;
