68003ebb63c2d5fe43cf9311f1649b5f2bf8e435
Alexey Gasenko <gboydf@yandex.ru>
2019-05-31 01:26:42 +0500

    Comment submitting

--- routes/comments.js
+++ routes/comments.js
@@ -13,4 +13,25 @@ router.get('/:url', function(req, res) {
     });
 });
 
+router.post('/', function(req, res) {
+  const newComment = new Comment({
+    author: req.body.userId,
+    text: req.body.text,
+    url: req.body.url,
+    rating: 0,
+    date: Date.now()
+  });
+
+  newComment.save()
+    .then(() => {
+      Comment.find({
+        url: req.body.url,
+      })
+        .populate('author')
+        .then(comments => {
+          res.status(200).send(comments);
+        });
+    })
+});
+
 module.exports = router;
